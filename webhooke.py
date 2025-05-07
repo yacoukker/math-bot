1f0af8ca12ade525b95df5908364c3f3b767c4ff
yacine oukker <125767385+yacoukker@users.noreply.github.com>
2025-05-07 20:37:52 +0100

    Update webhooke.py

--- webhooke.py
+++ webhooke.py
@@ -48,4 +48,4 @@ def webhook():
     return jsonify({'fulfillmentText': message})
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
